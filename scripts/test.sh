5ad4e5e656353f3f59f0c951890f55f8856e6e3d
Luke Chavers <luke@c2cschools.com>
2016-02-25 22:52:29 -0600

    Added test script placeholder

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,17 +3,5 @@
 # This script will execute the unit tests for this project.
 
 echo ""
-echo "Running project tests"
-echo "---------------------------"
+echo "No tests yet.."
 echo ""
-
-# Ensure proper directory
-cd /project
-
-# Run the tests with the console renderer
-mocha
-
-# Run the tests with the html renderer
-mkdir -p ./doc/tests
-mocha --reporter doc > doc/tests/index.html
-
