05fc6094949f9e3fd1194c27387d35001928b67d
Luke Chavers <luke@c2cschools.com>
2016-02-25 22:00:19 -0600

    Improved Vagrant Provisioning

--- env/vagrant/always.sh
+++ env/vagrant/always.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-# This file is executed each time that "vagrant up" or "vagrant reload" is
-# executed.  During provisioning, this file is executed AFTER provision.sh.
 
-# Load common path variables
-source "/project/env/vagrant/provision/_paths.sh"
 
-echo ""
-echo "[Always] always.sh: Rebuilding YUM Cache"
-yum makecache fast
+# This script is executed by Vagrant each time the machine is booted.
+# On the first boot it will run immediately after provision.sh.
 
-echo ""
-echo "[Always] always.sh: Updating 'locate' Database (disabled)"
-# updatedb
 
-echo ""
+
+# Find my script location
+PROVISION_SCRIPT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Settings
+GITHUB_SCRIPT_USER="vmadman"
+GITHUB_SCRIPT_REPO="linux-scripts"
+GITHUB_SCRIPT_BRANCH="master"
+GITHUB_SCRIPT_PATH="vagrant/centos7/dev/v1"
+
+# Include the GitHub Execution Helper
+source "$PROVISION_SCRIPT_ROOT/github-exec.sh"
+
+# Run Several Scripts from GitHub Files..
+exec_github_script "yum-update-cache"
+exec_github_script "mlocate-update-db"
